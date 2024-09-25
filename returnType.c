ac903ff5d4a7f4bf32f2a36fa8136361e7d9f95c
isuri rathnayake <tmsisuri.rathnayake@gmail.com>
2024-09-25 20:56:20 +0530

    return type in C

---
+++ returnType.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() { //return type
+
+    int i = 4; //data type (primitive)
+    int j = 3;
+    int k = i + j;
+    printf("The addition of %d and %d is %d",i,j,k); //printf --> print format
+
+    return 0;
+}
