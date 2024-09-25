49e8edb0d592411b0d54f5519e52a7f6c8a4cc2d
isuri rathnayake <tmsisuri.rathnayake@gmail.com>
2024-09-25 20:40:50 +0530

    variables in C

---
+++ variable.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    
+    int i = 4;
+    int j = 3;
+    int k = i + j;
+    printf("The addition of %d and %d is %d",i,j,k); //printf --> print format
+
+    return 0;
+}
