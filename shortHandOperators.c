ca919d5c1fd5db014c68914367ac955ca43f3340
isuri rathnayake <tmsisuri.rathnayake@gmail.com>
2025-02-07 11:42:52 +0530

    short hand operators in c

---
+++ shortHandOperators.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+
+    int i = 9;
+
+    i += 7;
+    printf("The addition is %d\n",i);
+
+    i -= 4;
+    printf("The subtraction is %d\n",i);
+
+    i *= 5;
+    printf("The multiplication is %d\n",i);
+
+    i /= 3;
+    printf("The division is %d\n",i);
+
+    return 0;
+}
