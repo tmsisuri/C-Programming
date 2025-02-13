788969b3fbde61b8f7def86e01f4323389e5171e
isuri rathnayake <tmsisuri.rathnayake@gmail.com>
2025-02-13 16:44:57 +0530

    nested for loop in c

---
+++ nestedForLoop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    
+    for(int j=1; j<=4; j++) {
+        for(int i=1; i<=5; i++) {
+            printf("* "); 
+        }
+        printf("\n"); 
+    }
+
+    return 0;
+}
