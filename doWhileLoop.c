0b3d21af15540d3639103c01730cfde3fc1ecf03
isuri rathnayake <tmsisuri.rathnayake@gmail.com>
2025-02-13 16:33:28 +0530

    do while loop in c

---
+++ doWhileLoop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int i = 6; // initialization
+
+    do { 
+        printf("Hi\n"); 
+        i++;  //increment or decrement
+    } 
+    while(i<=5); //condition
+
+    return 0;
+}
