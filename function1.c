e8b4117d7fa0afa916ae668515fb44ac1da2ced5
isuri rathnayake <tmsisuri.rathnayake@gmail.com>
2025-02-15 21:14:58 +0530

    user defined function

---
+++ function1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void show();  //Declaration
+
+int main() {
+
+    /*
+        Declaration
+        Definition
+        Callling
+    */
+
+    show();  //Callling
+    return 0;
+}
+
+void show(){
+    printf("function");  //Definition
+}
