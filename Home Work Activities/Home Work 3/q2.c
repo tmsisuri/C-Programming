d4c55c9b06e765a1c6ffb4e999c08c802e29f840
isuri rathnayake <tmsisuri.rathnayake@gmail.com>
2025-02-22 19:07:06 +0530

    2nd commit

--- Home Work Activities/Home Work 3/q2.c
+++ Home Work Activities/Home Work 3/q2.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 int main() {
-    int num, p;
+
+    int num, Count;
+
     printf("Enter a number : ");
     scanf("%d", &num);
+
     for(int i=1; i<=num; i++) {
         if(num%i==0) {
-            p++;
+            Count++;
         }
     }
-    if(p==2)
+    
+    if(Count==2)
         printf("Entered number is a prime number.");
     else
         printf("Entered number is not a prime number.");
