dd074ad9b27c62d3e858f57f751bbf1461c4e806
isuri rathnayake <tmsisuri.rathnayake@gmail.com>
2025-02-24 18:45:11 +0530

    commit 2

--- Home Work Activities/Home Work 4/q9.c
+++ Home Work Activities/Home Work 4/q9.c
@@ -53,10 +53,11 @@ int multiplication(int num1, int num2) {
     return num1 * num2;
 }
 int division(int num1, int num2) {
-    if(num1!=0) {
+    if(num2!=0) {
         return num1 / num2;
     } else {
-        printf("can't devid by zero");
+        printf("can't devide by zero\n");
+        return 0;
     }
 }
 
