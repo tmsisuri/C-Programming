26a037624a93f165eb5446c3cbe6fb0d7bf3e749
isuri rathnayake <tmsisuri.rathnayake@gmail.com>
2025-02-25 21:31:23 +0530

    question 1

---
+++ Practicals/Practical 01/q1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main() {
+
+    printf("\t\t\t<<<WHALES>>>\n");
+
+    printf("\"Whales are the largest animals on Earth and they live in every ocean.\n"
+           "The massive mammals range from the 600-pound dwarf sperm whale to the \n"
+           "colossal blue whale, which can weigh more than 200 tons and stretch up to 100 \n"
+           "feet long—almost as long as a professional basketball court.\"\n\n");
+
+    printf("* C O M M O N N A M E : \n");
+    printf("\t Whales \n\n");
+
+    printf("* S C I E N T I F I C N A M E : \n");
+    printf("\t Cetacea \n\n");
+
+    printf("* D I E T : \n");
+    printf("\tCarnivore \n\n");
+
+    printf("* A V E R A G E L I F E S P A N : \n");
+    printf("\t unknown \n\n");
+
+    printf("* S I Z E : \n");
+    printf("\t 9 feet to 98 feet long \n\n");
+
+    printf("* W E I G H T : \n");
+    printf("\t 500 pounds to 200 tons \n\n");
+
+    return 0;
+}
